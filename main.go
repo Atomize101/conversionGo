490440b0fb02b96606ba18626c91b1c3dd48233e
Chris Patrick <atomize101@gmail.com>
2021-07-05 12:56:06 -0400

    unmarshal and format the JSON info

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
+
+type Result struct {
+	Success   bool
+	Timestamp int
+	Base      string
+	Date      string
+	Rates     map[string]float64
+}
+
+type Error struct {
+	Success bool
+	Error   struct {
+		Code int
+		Type string
+		Info string
+	}
+}
 
 func main() {
 	url := "http://data.fixer.io/api/latest?access_key=49fd52932dfdb84310a3dcfa77c25beb"
@@ -13,10 +31,20 @@ func main() {
 	if resp, err := http.Get(url); err == nil {
 		defer resp.Body.Close()
 		if body, err := ioutil.ReadAll(resp.Body); err == nil {
-			fmt.Println(string(body))
-		} else {
-			log.Fatal(err)
+			var result Result
+			json.Unmarshal([]byte(body), &result)
+			if result.Success {
+				for i, v := range result.Rates {
+					fmt.Println(i, v)
+				}
+			} else {
+				var err Error
+				json.Unmarshal([]byte(body), &err)
+				fmt.Println(err.Error.Info)
+			}
 		}
-		fmt.Println("Done")
+	} else {
+		log.Fatal(err)
 	}
+	fmt.Println("Done")
 }
